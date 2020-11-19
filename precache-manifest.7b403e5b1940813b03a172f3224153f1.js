d59aaa94083c99bfadfa16b5da951dcbdf4068cd
jmonteer <jmonteer@protonmail.com>
2020-11-19 19:33:27 +0100

    Updates

---
+++ precache-manifest.7b403e5b1940813b03a172f3224153f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbaa020fdf5067f99154e1b8abdcd629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9440862730b710a7bc09",
+    "url": "/static/css/main.e18ada2f.chunk.css"
+  },
+  {
+    "revision": "5f21003d895ac306cdcc",
+    "url": "/static/js/2.af3efcde.chunk.js"
+  },
+  {
+    "revision": "8f4b65a3363cf6cb54241b91b72981e9",
+    "url": "/static/js/2.af3efcde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9440862730b710a7bc09",
+    "url": "/static/js/main.e57ceb67.chunk.js"
+  },
+  {
+    "revision": "f8f36830675569a39379",
+    "url": "/static/js/runtime-main.a436498d.js"
+  }
+]);
